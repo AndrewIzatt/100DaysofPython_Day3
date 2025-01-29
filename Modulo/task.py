a1a1949a9d0ad29ad6f4224ea139ffdd2da5f847
Andrew Izatt <izatt.andrew@gmail.com>
2025-01-28 18:54:51 -0700

    Complete assignment

---
+++ Modulo/task.py
@@ -0,0 +1,5 @@
+my_number = int(input("Please enter a number\n"))
+if my_number % 2 == 0:
+    print("Your number is even.")
+else:
+    print("Your number is odd.")
--- Python Pizza/task.py
+++ Python Pizza/task.py
@@ -2,4 +2,21 @@ print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
+# calculate price of size pizza
+if size == 'S':
+    price = 15
+elif size == 'M':
+    price = 20
+else:
+    price = 25
 
+if extra_cheese == 'Y':
+    price += 1
+
+if pepperoni == 'Y':
+    if size == 'M' or size == 'L':
+        price += 3
+    else:
+        price += 2
+
+print(f"Your final bill is: ${price}.")
